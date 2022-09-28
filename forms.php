996655478b999ac5c841e366671e7d2581690c05
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-09-28 11:14:05 +0300

    Update forms.php

--- forms.php
+++ forms.php
@@ -2,13 +2,16 @@
 
 <body>
 
-<form action="welcome.php" method="post">
+<form action="forms.php" method="post">
 
 Name: <input type="text" name="name"><br>
 
 E-mail: <input type="text" name="email"><br>
 
 <input type="submit">
+Welcome <?php echo $_POST["name"]; ?><br>
+
+Your email address is: <?php echo $_POST["email"]; ?>
 
 </form>
 
