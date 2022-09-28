58bcd952dfa0a9fe1ce17c7a83358c5ed0a9a002
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-09-28 17:25:17 +0300

    Update string methods

--- strings.php
+++ strings.php
@@ -3,4 +3,8 @@ $myString "I Love PHP";
 strlen($myString) // outputs 10
 strrev($myString) // outputs PHP evol I
 str_word_count($myString) // outputs 3
+
+//strpos() - search for text within a string
+//str_replacr -replace text within string
+
 ?>
