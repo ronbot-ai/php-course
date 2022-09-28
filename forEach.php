1bb6849259ecc683048b086b82a0d618ab9d8678
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-09-29 02:32:13 +0300

    added key value looping

--- forEach.php
+++ forEach.php
@@ -4,4 +4,10 @@ $colors = array("red", "green", "blue", "yellow");
 foreach ($colors as $value) {
   echo "$value <br>";
 }
+// Key value looping
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach($age as $x => $val) {
+  echo "$x = $val<br>";
+}
 ?>
