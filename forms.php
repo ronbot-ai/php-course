9e8c80ea4cdb089321f03e31769b92a14793d7d1
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-09-28 11:21:11 +0300

    Added get method code

--- forms.php
+++ forms.php
@@ -1,7 +1,7 @@
 <html>
 
 <body>
-
+//Post method 
 <form action="forms.php" method="post">
 
 Name: <input type="text" name="name"><br>
@@ -9,11 +9,21 @@ Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 
 <input type="submit">
+//Php Code snippet 
 Welcome <?php echo $_POST["name"]; ?><br>
 
 Your email address is: <?php echo $_POST["email"]; ?>
 
 </form>
+//Get method
+<form action="form.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+// Php code snippet 
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
 
 </body>
 
